d978e045d1799ea8de1decbfe725c42d4aedaea3
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-07-25 22:10:48 +0700

    Add files via upload

---
+++ TH.Hệ Quản Trị CSDL/Lab 2/Lab 2 - Bài Tập Có HD.sql
@@ -0,0 +1,40 @@
+﻿--1: Truy vấn danh sách sinh viên cùng với tên khoa của họ:
+select MASV,HOSV,TENSV,TENKH
+from SinhVien inner join khoa on SinhVien.MAKH=khoa.MAKH
+--2: Truy vấn danh sách sinh viên và điểm số của họ trong từng môn học:
+select SinhVien.MASV,TENSV,TENMH,DIEM
+from SinhVien inner join KetQua on SinhVien.MASV=KetQua.MASV
+			inner join MonHoc on KetQua.MAMH=MonHoc.MAMH
+--3: Truy vấn danh sách sinh viên có học bổng từ 1,000,000 đồng trở lên.
+select MASV,HOSV,TENSV, HOCBONG
+from SinhVien
+where HOCBONG>1000000
+--4: Truy vấn tên các môn học cùng với số lượng sinh viên đăng ký học mỗi môn:
+select TENMH,SLSV = count (MASV)
+from MonHoc inner join KetQua on MonHoc.MAMH=KetQua.MAMH
+group by TENMH
+--5: Truy vấn danh sách sinh viên và điểm trung bình của họ trong các môn học:
+select SinhVien.MASV,HOSV,TENSV,DiemTB=AVG(Diem)
+from SinhVien inner join KetQua on SinhVien.MASV=KetQua.MASV
+group by SinhVien.MASV,HOSV,TENSV
+--6: Truy vấn danh sách các khoa và số lượng sinh viên trong mỗi khoa.
+select TENKH,SLSV=count(MASV)
+from SinhVien inner join khoa on SinhVien.MAKH=khoa.MAKH
+group by TENKH
+--7: Truy vấn danh sách sinh viên và số lượng môn học mà mỗi sinh viên đã đăng ký học.
+select SinhVien.MASV,HOSV,TENSV,SLMH=count(MonHoc.MAMH)
+from SinhVien inner join KetQua on SinhVien.MASV=KetQua.MASV
+			inner join MonHoc on KetQua.MAMH=MonHoc.MAMH
+group by SinhVien.MASV,HOSV,TENSV
+--8: Truy vấn danh sách sinh viên có điểm môn "Toán" lớn hơn 8.
+select SinhVien.MASV,HOSV,TENSV
+from SinhVien inner join KetQua on SinhVien.MASV=KetQua.MASV
+inner join MonHoc on KetQua.MAMH=MonHoc.MAMH
+where TENMH=N'Toán'and DIEM>8
+--9: Truy vấn danh sách sinh viên và tuổi của họ (dựa trên ngày sinh).
+select MASV,HOSV,TENSV,Tuoi=YEAR(getdate())-YEAR(NGAYSINH)
+from SinhVien
+--10: Truy vấn danh sách sinh viên và điểm cao nhất mà mỗi sinh viên đã đạt được.
+select SinhVien.MASV,HOSV,TENSV,DiemCaoNhat=MAX(DIEM)
+from SinhVien inner join KetQua on SinhVien.MASV=KetQua.MASV
+group by SinhVien.MASV,HOSV,TENSV
---
+++ TH.Hệ Quản Trị CSDL/Lab 2/Lab 2 - Bài Tập Tự TH.sql
@@ -0,0 +1,111 @@
+﻿--1: Liệt kê tất cả các nhân viên và thông tin chi tiết của họ.
+select *
+from NHANVIEN
+--2: Tính tổng lương (bao gồm lương cơ bản, lương thưởng, và phụ cấp) của tất cả các nhân viên trong tháng 6 năm 2023.
+select NHANVIEN.MANV,HOTEN, TongLuong = LUONGCB*HESOLUONG+LUONGTHUONG+PHUCAP
+from NHANVIEN inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+where MONTH(NGAY)=6 and YEAR(NGAY)=2023
+--3: Liệt kê các nhân viên thuộc phòng ban "Phòng Kỹ Thuật".
+select MANV,HOTEN
+from NHANVIEN inner join PHONGBAN on NHANVIEN.MAPB = PHONGBAN.MAPB
+where TENPB=N'Phòng Kỹ Thuật'
+--4: Tìm kiếm thông tin nhân viên theo mã nhân viên.
+select MANV,HOTEN
+from NHANVIEN
+where MANV='NV02'
+--5: Liệt kê các nhân viên có chức vụ "Trưởng Phòng".
+select MANV,HOTEN
+from NHANVIEN inner join CHUCVU on NHANVIEN.MACV=CHUCVU.MACV
+where TENCV=N'Trưởng Phòng'
+--6: Tính tổng số nhân viên trong mỗi phòng ban.
+select PHONGBAN.MAPB,TENPB,SLNV=COUNT(MANV)
+from NHANVIEN inner join PHONGBAN on NHANVIEN.MAPB = PHONGBAN.MAPB
+group by PHONGBAN.MAPB,TENPB
+--7: Liệt kê thông tin lương của tất cả nhân viên theo từng ngày.
+select *
+from LUONG
+--8: Cập nhật địa chỉ của một nhân viên.
+update NHANVIEN 
+set DIACHI=N'Sài Gòn'
+where MANV='NV03'
+--9: Xóa thông tin của một nhân viên theo mã nhân viên.
+delete NHANVIEN
+where MANV='NV02'
+--10: Thêm một nhân viên mới vào bảng NHANVIEN.
+insert into NHANVIEN values ('NV04',N'Định Bồn Nước','2004-10-26',1,N'Quảng Ngãi','PB01','CV01',10000000,1.4)
+--11: Truy vấn tên các chức vụ và số lượng nhân viên trong từng chức vụ
+select TENCV,SLNV=COUNT(MANV)
+from NHANVIEN inner join CHUCVU on NHANVIEN.MACV=CHUCVU.MACV
+group by TENCV
+--12: Truy vấn các nhân viên và tổng lương trong tháng 6/2023, sắp xếp theo tổng lương giảm dần
+select NHANVIEN.MANV,HOTEN, TongLuong = LUONGCB*HESOLUONG+LUONGTHUONG+PHUCAP
+from NHANVIEN inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+where MONTH(NGAY)=6 and YEAR(NGAY)=2023
+order by TongLuong DESC
+--13: Truy vấn danh sách nhân viên và phòng ban mà nhân viên đó thuộc về, chỉ lấy các
+--nhân viên thuộc phòng "Phòng Kỹ Thuật"
+select MANV,HOTEN,TENPB
+from NHANVIEN inner join PHONGBAN on NHANVIEN.MAPB = PHONGBAN.MAPB
+where TENPB=N'Phòng Kỹ Thuật'
+--14: Truy vấn danh sách các nhân viên, chức vụ và tổng lương, chỉ lấy các nhân viên có
+--tổng lương trên 8 triệu
+select NHANVIEN.MANV,HOTEN,TENCV, TongLuong = LUONGCB*HESOLUONG+LUONGTHUONG+PHUCAP
+from NHANVIEN inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+inner join CHUCVU on NHANVIEN.MACV=CHUCVU.MACV
+where LUONGCB*HESOLUONG+LUONGTHUONG+PHUCAP>8000000
+--15: Truy vấn tổng số nhân viên và lương trung bình theo phòng ban
+select PHONGBAN.MAPB,TENPB,SLNV=COUNT(NHANVIEN.MANV),LuongTB=AVG(LUONGCB*HESOLUONG+LUONGTHUONG+PHUCAP)
+from NHANVIEN inner join PHONGBAN on NHANVIEN.MAPB = PHONGBAN.MAPB
+inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+group by PHONGBAN.MAPB,TENPB
+--Ngoại lệ:
+insert into LUONG values ('NV04','2023-06-01',800000,600000)
+--16: Truy vấn các phòng ban có nhiều hơn 1 nhân viên
+select PHONGBAN.MAPB,TENPB,SLNV=COUNT(NHANVIEN.MANV)
+from NHANVIEN inner join PHONGBAN on NHANVIEN.MAPB = PHONGBAN.MAPB
+group by PHONGBAN.MAPB,TENPB
+having COUNT(NHANVIEN.MANV)>1
+--17: Truy vấn tổng số nhân viên và lương trung bình theo giới tính, chỉ lấy những
+--nhóm có lương trung bình lớn hơn 6 triệu
+select GIOITINH = case when GIOITINH=0 then N'Nữ' else 'Nam' end, SLNV=COUNT(NHANVIEN.MANV),LuongTB=AVG(LUONGCB*HESOLUONG+LUONGTHUONG+PHUCAP)
+from NHANVIEN inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+group by GIOITINH
+having AVG(LUONGCB*HESOLUONG+LUONGTHUONG+PHUCAP)>6000000
+--18: Truy vấn tổng lương thưởng và phụ cấp của mỗi nhân viên
+select NHANVIEN.MANV,HOTEN,TongLuong=LUONGTHUONG+PHUCAP
+from NHANVIEN inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+--19: Truy vấn tổng lương thưởng và phụ cấp theo phòng ban, chỉ lấy những phòng ban
+--có tổng phụ cấp lớn hơn 500000
+select PHONGBAN.MAPB,TENPB,TongLuong=SUM(LUONGTHUONG+PHUCAP)
+from NHANVIEN inner join PHONGBAN on NHANVIEN.MAPB = PHONGBAN.MAPB
+inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+group by PHONGBAN.MAPB,TENPB
+having SUM(LUONGTHUONG+PHUCAP)>500000
+--20: Truy vấn số lượng nhân viên theo từng năm sinh
+select 'Năm Sinh'=YEAR(NGAYSINH), SLNV=COUNT(MANV)
+from NHANVIEN
+group by YEAR(NGAYSINH)
+--21: Truy vấn số lượng nhân viên theo năm sinh, chỉ lấy những năm có nhiều hơn 1 nhân viên
+select 'Năm Sinh'=YEAR(NGAYSINH), SLNV=COUNT(MANV)
+from NHANVIEN
+group by YEAR(NGAYSINH)
+having COUNT(MANV)>1
+--22: Truy vấn tổng lương (lương cơ bản + lương thưởng + phụ cấp) theo chức vụ, sắp xếp theo tổng lương giảm dần
+select CHUCVU.MACV,TENCV, TongLuong = SUM(LUONGCB*HESOLUONG+LUONGTHUONG+PHUCAP)
+from NHANVIEN inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+inner join CHUCVU on NHANVIEN.MACV=CHUCVU.MACV
+group by CHUCVU.MACV,TENCV
+order by TongLuong desc
+--23: Truy vấn số lượng nhân viên và tổng lương cơ bản theo giới tính, chỉ lấy những
+--nhóm có tổng lương cơ bản trên 15 triệu
+select GIOITINH = case when GIOITINH=0 then N'Nữ' else 'Nam' end, SLNV=COUNT(NHANVIEN.MANV),LuongTB=SUM(LUONGCB)
+from NHANVIEN inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+group by GIOITINH
+having SUM(LUONGCB)>15000000
+--24: Truy vấn tổng lương thưởng và phụ cấp của nhân viên trong mỗi phòng ban, chỉ
+--lấy những phòng ban có tổng lương thưởng trên 1 triệu
+select PHONGBAN.MAPB,TENPB,TongLuongThuong=SUM(LUONGTHUONG),PhuCap=SUM(PHUCAP)
+from NHANVIEN inner join PHONGBAN on NHANVIEN.MAPB = PHONGBAN.MAPB
+inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+group by PHONGBAN.MAPB,TENPB
+having SUM(LUONGTHUONG)>1000000
