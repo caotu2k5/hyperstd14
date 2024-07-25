350c6e3ce853f51b9a5cb38600b7967ec45d88ed
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-07-25 22:12:19 +0700

    Add files via upload

---
+++ TH.Hệ Quản Trị CSDL/Lab 4/Lab 4 - Bài tập có hướng dẫn.sql
@@ -0,0 +1,159 @@
+﻿--C1: Tạo Stored Procedure để thêm sinh viên mới vào bảng SINHVIEN.
+create procedure ThemSinhVien
+	@MASV char(3),
+	@HOSV nvarchar(15),
+	@TENSV nvarchar(7),
+	@PHAI bit,
+	@NGAYSINH datetime,
+	@NOISINH nvarchar(15),
+	@MAKH char(2),
+	@HOCBONG INT
+as
+begin
+if exists (select 1 from SinhVien where MASV=@MASV)
+	begin
+		print N'Mã sinh viên đã tồn tại'
+	end
+else 
+	begin
+	insert into SinhVien (MASV,HOSV,TENSV,PHAI,NGAYSINH,NOISINH,MAKH,HOCBONG)
+		values (@MASV,@HOSV,@TENSV,@PHAI,@NGAYSINH,@NOISINH,@MAKH,@HOCBONG)
+		print N'Thêm sinh viên thành công'
+	end
+end
+--Chạy thực thi
+exec ThemSinhVien
+	@MASV ='A01',
+	@HOSV=N'Trần',
+	@TENSV=N'Dần',
+	@PHAI=1,
+	@NGAYSINH='2000-01-01',
+	@NOISINH=N'TP.HCM',
+	@MAKH='CS',
+	@HOCBONG=100000;
+
+select * from sinhvien
+
+--C2: Tạo Stored Procedure để cập nhật thông tin sinh viên.
+create procedure CapNhatSinhVien
+	@MASV char(3),
+	@HOSV nvarchar(15),
+	@TENSV nvarchar(7),
+	@PHAI bit,
+	@NGAYSINH datetime,
+	@NOISINH nvarchar(15),
+	@MAKH char(2),
+	@HOCBONG INT
+as
+begin
+if not exists (select 1 from SinhVien where MASV=@MASV)
+	begin
+		print N'Mã sinh viên không tồn tại'
+	end
+else 
+	begin
+	update SinhVien
+	set HOSV=@HOSV,
+	TENSV=@TENSV,
+	PHAI=@PHAI,
+	NGAYSINH=@NGAYSINH,
+	NOISINH=@NOISINH,
+	MAKH=@MAKH,
+	HOCBONG=@HOCBONG
+		where MASV=@MASV
+		print N'Cập nhật sinh viên thành công'
+	end
+end
+--Chạy thực thi
+exec CapNhatSinhVien
+	@MASV ='A01',
+	@HOSV=N'Trần',
+	@TENSV=N'Cọp',
+	@PHAI=1,
+	@NGAYSINH='2000-01-01',
+	@NOISINH=N'TP.HCM',
+	@MAKH='CS',
+	@HOCBONG=100000;
+
+select * from SinhVien
+--3: Tạo Stored Procedure để xóa sinh viên khỏi bảng SINHVIEN.
+create procedure XoaSinhVien
+	@MASV char(3),
+	@HOSV nvarchar(15),
+	@TENSV nvarchar(7),
+	@PHAI bit,
+	@NGAYSINH datetime,
+	@NOISINH nvarchar(15),
+	@MAKH char(2),
+	@HOCBONG INT
+as
+begin
+if not exists (select 1 from SinhVien where MASV=@MASV)
+	begin
+		print N'Mã sinh viên không tồn tại'
+	end
+else 
+	begin
+	delete SinhVien
+		where MASV=@MASV
+		print N'Xóa sinh viên thành công'
+	end
+end
+--Chạy thực thi
+exec XoaSinhVien
+	@MASV ='A01',
+	@HOSV=N'Trần',
+	@TENSV=N'Cọp',
+	@PHAI=1,
+	@NGAYSINH='2000-01-01',
+	@NOISINH=N'TP.HCM',
+	@MAKH='CS',
+	@HOCBONG=100000;
+
+--4: Tạo Stored Procedure để lấy thông tin chi tiết của một sinh viên.
+create procedure LayThongTinSV
+	@MASV char(3)
+as
+begin
+	if not exists (select 1 from SinhVien where MASV=@MASV)
+	begin
+		print N'Mã sinh viên không tồn tại'
+	end
+else 
+	begin
+	select *
+	from SinhVien
+	where MASV=@MASV
+	end
+end
+--Chạy thực thi
+exec LayThongTinSV
+	@MASV ='A01'
+
+--5: Tạo Stored Procedure để tính điểm trung bình của sinh viên.
+create procedure DiemTrungBinhSV
+	@MASV char(3),
+	@DiemTB float output
+as
+begin
+	if not exists (select 1 from SinhVien where MASV=@MASV)
+	begin
+		print N'Mã sinh viên không tồn tại'
+	end
+else 
+	begin
+	select @DiemTB=AVG(diem)
+	from KetQua
+	where MASV=@MASV
+	PRINT N'Điểm trung bình của sinh viên ' + @MASV +' là: ' + CAST(@DiemTB AS
+NVARCHAR)
+	end
+end
+--Chạy thực thi
+declare @DiemTB float;
+exec DiemTrungBinhSV
+	@MASV ='001',
+	@DiemTB=@DiemTB output
+--...
+	select @DiemTB as DiemTB
+
---
+++ TH.Hệ Quản Trị CSDL/Lab 4/Lab 4 - Bài tập tự thực hành.sql
@@ -0,0 +1,148 @@
+﻿--1: Tạo Stored Procedure để thêm nhân viên mới vào bảng NHANVIEN.
+create procedure ThemNhanVien
+	@MANV char(5),
+	@HOTEN nvarchar(50),
+	@NGAYSINH datetime,
+	@GIOITINH bit,
+	@DIACHI nvarchar(100),
+	@MAPB char (5),
+	@MACV char(5),
+	@LUONGCB float,
+	@HESOLUONG float
+as
+begin
+	if exists (select 1 from NHANVIEN where @MANV=MANV)
+		begin
+			print N'Mã nhân viên đã tồn tại'
+		end
+	else
+		begin
+			insert into NHANVIEN (MANV, HOTEN, NGAYSINH, GIOITINH, DIACHI,MAPB, MACV, LUONGCB, HESOLUONG)
+			values (@MANV, @HOTEN, @NGAYSINH, @GIOITINH, @DIACHI,@MAPB, @MACV, @LUONGCB, @HESOLUONG)
+			print N'Thêm nhân viên thành công'
+		end
+end
+--Chạy thực thi
+exec ThemNhanVien
+	@MANV='001',
+	@HOTEN=N'Trần Dần',
+	@NGAYSINH='2000-03-03',
+	@GIOITINH=1,
+	@DIACHI=N'33/33 Nhà 33',
+	@MAPB='PB01',
+	@MACV='CV01',
+	@LUONGCB=10000000,
+	@HESOLUONG=1.5
+
+--2: Tạo Stored Procedure để cập nhật thông tin nhân viên.
+create procedure CapNhatNhanVien
+	@MANV char(5),
+	@HOTEN nvarchar(50),
+	@NGAYSINH datetime,
+	@GIOITINH bit,
+	@DIACHI nvarchar(100),
+	@MAPB char (5),
+	@MACV char(5),
+	@LUONGCB float,
+	@HESOLUONG float
+as
+begin
+	if not exists (select 1 from NHANVIEN where @MANV=MANV)
+		begin
+			print N'Mã nhân viên không tồn tại'
+		end
+	else
+		begin
+			update NHANVIEN
+			set HOTEN=@HOTEN,
+			NGAYSINH=@NGAYSINH,
+			GIOITINH=@GIOITINH,
+			DIACHI=@DIACHI,
+			MAPB=@MAPB,
+			MACV=@MACV,
+			LUONGCB=@LUONGCB,
+			HESOLUONG=@HESOLUONG
+			where MANV=@MANV
+			print N'Cập nhật thông tin nhân viên thành công'
+		end
+end
+--Chạy thực thi
+exec CapNhatNhanVien
+	@MANV='001',
+	@HOTEN=N'Trần Cọp',
+	@NGAYSINH='2000-03-03',
+	@GIOITINH=1,
+	@DIACHI=N'33/33 Nhà 33',
+	@MAPB='PB01',
+	@MACV='CV01',
+	@LUONGCB=10000000,
+	@HESOLUONG=1.5
+--3: Tạo Stored Procedure để xóa thông tin nhân viên.
+create procedure XoaNhanVien
+	@MANV char(5)
+as
+begin
+	if not exists (select 1 from NHANVIEN where @MANV=MANV)
+		begin
+			print N'Mã nhân viên không tồn tại'
+		end
+	else
+		begin
+			delete NHANVIEN
+			where MANV=@MANV
+			print N'Xóa thông tin nhân viên thành công'
+		end
+end
+--Chạy thực thi
+exec XoaNhanVien
+	@MANV='001'
+--4: Tạo Stored Procedure để tìm kiếm thông tin nhân viên theo mã nhân viên.
+create procedure TimKiemNhanVien
+	@MANV char(5)
+as
+begin
+	if not exists (select 1 from NHANVIEN where @MANV=MANV)
+		begin
+			print N'Mã nhân viên không tồn tại'
+		end
+	else
+		begin
+			select * from NHANVIEN
+			where MANV=@MANV
+		end
+end
+--Chạy thực thi
+exec TimKiemNhanVien
+	@MANV='001'
+--5: Tạo Stored Procedure để tính tổng lương của nhân viên trong một tháng.
+create procedure TongLuongNV
+	@MANV char(5),
+	@MONTH int,
+	@YEAR int,
+	@TongLuong decimal(18,1) output
+as
+begin
+	if not exists (select 1 from NHANVIEN where MANV=@MANV)
+	begin
+		print N'Mã Nhân viên không tồn tại'
+	end
+else 
+	begin
+	select @TongLuong=LUONGCB+LUONGTHUONG+PHUCAP
+	from NHANVIEN inner join LUONG on NHANVIEN.MANV=LUONG.MANV
+	where NHANVIEN.MANV=@MANV or LUONG.MANV=@MANV
+	PRINT N'Tổng lương của nhân viên ' + @MANV +' là: ' + CAST(@TongLuong AS
+VARCHAR(15))
+	end
+end
+--Chạy thực thi
+declare @TongLuong decimal(18,1);
+exec TongLuongNV
+	@MANV ='NV01',
+	@MONTH =6,
+	@YEAR=2023,
+	@TongLuong=@TongLuong output
+--...
+	select @TongLuong as TongLuong
+
+
